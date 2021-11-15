f711ff7091c74ea864385784adbf1cc136784db4
danielba94 <94314873+danielba94@users.noreply.github.com>
2021-11-14 22:50:01 -0500

    Create Game.cs (#1)

---
+++ Model/Game.cs
@@ -0,0 +1,8 @@
+public class Game{
+  private int _id;
+  private string _label;
+  private string _teamOne;
+  private string _teamTwo,
+  private DateTimeOffset _startTime;
+  private DateTimeOffset _endTime;
+  }
